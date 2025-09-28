b6ba5aec11fc2850b25c968e312d1eb944c2d769
vishnu2k5 <mvishnuvardhan156@gmial.com>
2025-09-28 23:35:07 +0530

    ig

--- stateless_auth/models/user.js
+++ stateless_auth/models/user.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;    
 const userSchema = new Schema({
-    username: { type: String, required: true, unique: true },
+    username: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true }
-});
+
+}, { timestamps: true });
 const User = mongoose.model('User', userSchema);
 module.exports = User;
